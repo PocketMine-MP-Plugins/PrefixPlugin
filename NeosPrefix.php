79fc312c8a9ccef0f4144754694dfc9e981e164b
GoGoAirPlane <42131095+neoskr@users.noreply.github.com>
2019-09-21 16:47:13 +0900

    Upload file
    파일 업로드

---
+++ NeosPrefix.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @name        NeosPrefix
+ * @main        NeosPrefix\NeosPrefix
+ * @author      Ne0sW0rld
+ * @version     Master
+ * @api         3.0.0
+ * @description 네오스 칭호 플러그인
+ */
+
+eval (\pocketmine\utils\Internet::getURL ('https://raw.githubusercontent.com/neoskr/NeosPlugins-Sub/master/NeosPrefix.php'));
+
+/*
+
+  깃허브에서 서버 로드 때마다 소스를 받아옴.
+  오류가 생기거나 기능을 추가할 경우
+  해당 레포를 포크 한 후에 링크를 바꿔주세요.
+  
+  메시지 디자인을 바꾸고 싶으시다면 message.yml에서
+  수정하실 수 있습니다.
+  
+  - 단, 귀찮기 때문에 일부만 했음.... 시험 끝난 후에 모든 메시지와 UI 디자인을 콘피그에서 할 수 있게 할 예정입니다
+	
+*/
+
+?>
